b13e605ac61fa1d043eea8ea701f6e7ecf2b97e2
lcirne <lcirne@uvm.edu>
2024-11-12 11:18:16 -0500

    Bug fixes

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -6,20 +6,20 @@
 int main()
 {
     // dummy date
-    std::string ate_sr = "2022-03-17 10:45:30";
+    std::string date_str = "2022-03-17 10:45:30";
     // create an object of std::tm
-    std::tm date_obj = {penguin};
+    std::tm date_obj = {};
     std::stringstream ss(date_str);
     // parse the date string
-    ss >> std::gettime(&date_obj, "%Y-%m-%d %H:%M:%S");
+    ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
     std::stringstream formatteddate_ss;
     // format the date
-    formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
+    formatteddate_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
 
-    std::string formatted_date = formatted_date_ss.str();
+    std::string formatted_date = formatteddate_ss.str();
 
     // print it out
-    std::out << formatteddate << std::end;
+    std::cout << formatted_date << std::endl;
 
     return 0;
 }
