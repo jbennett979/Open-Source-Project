150571408c6a8dfcaf8cf3bc3df66592f7295710
Luke Holmes <71648285+Holmes-rgb@users.noreply.github.com>
2024-11-12 10:35:17 -0500

    Luke Holmes made a comment

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+#Luke Holmes made a comment here
 date_str = "2022-03-17 10:45:30"
 date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
 formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
