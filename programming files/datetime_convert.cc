dc5439535fd5fe3190df6ffd33eccf997952b398
SylvanFranklin <sylvanfranklin@icloud.com>
2024-11-12 10:34:01 -0500

    updated commentary

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -5,14 +5,20 @@
 
 int main()
 {
+    // dummy date
     std::string date_str = "2022-03-17 10:45:30";
+    // create an object of std::tm
     std::tm date_obj = {};
     std::istringstream ss(date_str);
+    // parse the date string
     ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
     std::stringstream formatted_date_ss;
+    // format the date
     formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
+
     std::string formatted_date = formatted_date_ss.str();
 
+    // print it out
     std::cout << formatted_date << std::endl;
 
     return 0;
